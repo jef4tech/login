f4de07dd54ba5e23d6a572212664854bd12ca6b8
Jeffin <jef4tech@gmail.com>
2019-05-16 16:10:45 +0530

    bakbutton

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void onBackPressed() {
+
+
+        Intent i = new Intent(MainActivity.this, login.class);
+        MainActivity.this.finish();
+        startActivity(i);
     }
 }
